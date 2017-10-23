5f73a63688e01162f19ea0abe49aaf272d1bbfcf
[Chauncey Frend] <[cfrend@iu.edu]>
2017-10-23 00:04:02 -0400

    IndiGoGo pre-launch updates

--- src/app/components/Order.js
+++ src/app/components/Order.js
@@ -13,22 +13,22 @@ export class Order extends React.Component {
                                            <div className="col-md-4">
                                                 <div className="text-cyutilWhite">
                                                       <div className="text-right">
-                                                            <p>cy.PIPES™ beta test units will be available starting in mid-October 2017 on Indigogo. Beta test units will only be sold in the US for now.</p>
+                                                            <p>cy.PIPES™ Early Adopter units will be available for pre-order on Indigogo before the end of the month. These test units will only be sold in the US for now.</p>
                                                             <h1><span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></h1>
                                                       </div>
                                                 </div>
                                           </div>
                                           <div className="col-md-4">
                                                       <div className="embed-responsive embed-responsive-16by9">
-                                                            <iframe width="360" height="203" src="https://www.youtube.com/embed/v7Bv2loQWmw" allowFullScreen></iframe>
+                                                            <iframe width="360" height="203" src="https://youtu.be/OpDxGwWjQIg" allowFullScreen></iframe>
                                                       </div>
                                                 <div className="text-center text-cyutilWhite">
-                                                      <p>A message from Chauncey about availability will be posted here from time to time.</p>
+                                                      <p>To be notified on the availability please sign up for updates on this <a href="https://www.indiegogo.com/projects/cy-pipes-early-adopter-offering/coming_soon">IndiGoGo</a> pre-launch page.</p>
                                                 </div>
                                           </div>
                                           <div className="col-md-4">
                                                 <div className="text-cyutilWhite text-left">
-                                                            <p>Details and announcements will be posted here and on <a href="https://www.facebook.com/Cyutil/">facebook</a> when ready.</p>
+                                                            <p>Details and announcements will also be posted on <a href="https://www.facebook.com/Cyutil/">Facebook</a>.</p>
                                                             <h1><span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span></h1>
                                                 </div>
                                           </div>
